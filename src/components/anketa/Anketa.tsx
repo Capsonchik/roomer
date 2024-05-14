ca1a553c3e7e4389e317dd444bc0a58e1f17d715
Capsonchik <Serega00790@mail.ru>
2024-05-14 10:03:40 +0300

    Хот фикс 5

--- src/components/anketa/Anketa.tsx
+++ src/components/anketa/Anketa.tsx
@@ -1,14 +1,12 @@
 import styles from './styles.module.css'
 import {Button, Container, Panel} from "rsuite";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {AppDispatch} from "@/store/store";
 import {setIsNewUser} from "@/store/main/mainSlice";
 import {TableComponent} from "@/components/table/TableComponent";
-import {selectUsers} from "@/store/main/mainSlice.selectors";
 
 export const Anketa = () => {
   const dispatch = useDispatch<AppDispatch>()
-  const users = useSelector(selectUsers)
 
   return (
     <Container>
